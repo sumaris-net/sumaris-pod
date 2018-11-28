a4aec27da2a29e24f33abcf77bd5fe207e987751
blavenie <benoit.lavenier@e-is.pro>
2018-11-28 12:34:13 +0100

    [fix] hsqldb script, and javaVersion in the pom.xml

--- sumaris-core/src/test/scripts/stopServer.sh
+++ sumaris-core/src/test/scripts/stopServer.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 export M2_REPO=$HOME/.m2/repository
-export CLASSPATH=$M2_REPO/org/hsqldb/hsqldb/2.4.0/hsqldb-2.4.0.jar
-export CLASSPATH=$CLASSPATH:$M2_REPO/org/hsqldb/sqltool/2.4.0/sqltool-2.4.0.jar
+export CLASSPATH=$M2_REPO/org/hsqldb/hsqldb/2.4.0/hsqldb-2.4.1.jar
+export CLASSPATH=$CLASSPATH:$M2_REPO/org/hsqldb/sqltool/2.4.1/sqltool-2.4.1.jar
 
-# install SqlTool 2.4.0
-mvn -q org.apache.maven.plugins:maven-dependency-plugin:2.1:get -DrepoUrl=http://download.java.net/maven/2/ -Dartifact=org.hsqldb:sqltool:2.4.0
+# install SqlTool 2.4.1
+mvn -q org.apache.maven.plugins:maven-dependency-plugin:2.1:get -DrepoUrl=http://download.java.net/maven/2/ -Dartifact=org.hsqldb:sqltool:2.4.1
 
 # Shutdown db-server
 java -classpath $CLASSPATH org.hsqldb.cmdline.SqlTool --rcFile=sqltool.rc --sql="shutdown;" sumaris
