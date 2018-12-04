84fa94877f3f577f6222cf76c873a4b2a1b26317
lp1ee9d <ludovic.pecquot@e-is.pro>
2018-12-04 16:06:23 +0100

    [add] add Windows batch in sumaris-core test scripts

---
+++ sumaris-core/src/test/scripts/restartServer.bat
@@ -0,0 +1,39 @@
+@echo off
+
+rem Init variables
+set SCRIPT_DIR=%cd%
+set PROJECT_DIR=..\..\..
+cd %PROJECT_DIR%
+set PROJECT_DIR=%cd%
+
+echo [93mInstalling [core-shared] and [test-shared]...[0m
+cd %PROJECT_DIR%\..
+call mvn install -DskipTests -pl sumaris-core-shared,sumaris-test-shared --quiet
+if not errorlevel 0 goto error
+
+echo [93mGenerating new test DB... (log at: %PROJECT_DIR%\target\build.log)[0m
+cd %PROJECT_DIR%
+rmdir /S /Q target\db 2>nul
+call mvn -Prun,hsqldb -DskipTests --quiet
+if not errorlevel 0 goto error
+
+echo [93mStopping DB server...[0m
+cd %SCRIPT_DIR%
+call stopServer.bat
+
+echo [93mCleaning old DB server files...[0m
+rmdir /S /Q %PROJECT_DIR%\target\db-server
+
+echo [93mStarting DB server...[0m
+cd %SCRIPT_DIR%
+start "SUMARiS HSQLDB SERVER" "startServer.bat"
+
+echo [92mOK[0m
+
+goto end
+
+:error
+echo [91mSomething bad happened[0m
+
+:end
+cd %SCRIPT_DIR%
--- sumaris-core/src/test/scripts/startServer.bat
+++ sumaris-core/src/test/scripts/startServer.bat
@@ -1,9 +1,33 @@
 @echo off
 
-SET M2_REPO=%HOMEPATH%%\.m2\repository\
+SET HSQLDB_VERSION=2.4.1
+SET M2_REPO=%HOMEDRIVE%%HOMEPATH%\.m2\repository\
+set CLASSPATH=%M2_REPO%\org\hsqldb\hsqldb\%HSQLDB_VERSION%\hsqldb-%HSQLDB_VERSION%.jar
 
-SET DB_DIRECTORY=../db-server
-SET DB_OPTS=--database.0 file:%DB_DIRECTORY%/sumaris --dbname.0 sumaris
+set DB_NAME=sumaris
+set TEST_DB=..\..\..\target\db
+set DB_DIRECTORY=..\..\..\target\db-server
+SET DB_OPTS=--database.0 file:%DB_DIRECTORY%/%DB_NAME% --dbname.0 %DB_NAME%
 
-rem -- DB server AND a temp DB:
-java -classpath %M2_REPO%\org\hsqldb\hsqldb\2.4.1\hsqldb-2.4.1.jar org.hsqldb.Server %DB_OPTS%
+rem make sure test DB exists
+if not exist "%TEST_DB%\%DB_NAME%.script" (
+    echo [91mTest DB not exists. Please run InitTest first ![0m
+    goto exit
+)
+
+rem Copy test DB
+echo [93mCopy test DB into 'target\db-server'[0m
+rmdir /S /Q %DB_DIRECTORY% 2>nul
+xcopy /S %TEST_DB% %DB_DIRECTORY%\
+rem Change 'readonly' value to false with PowerShell
+PowerShell -Command "(Get-Content %DB_DIRECTORY%\%DB_NAME%.properties).replace('readonly=true', 'readonly=false') | Set-Content %DB_DIRECTORY%\%DB_NAME%.properties"
+
+rem run DB server :
+echo [93mStartup HSQLDB Server [4m%HSQLDB_VERSION%[0m
+java -classpath %CLASSPATH% org.hsqldb.Server %DB_OPTS%
+
+rem exit this console here (eg. if the hsqldb is stopped)
+exit
+
+rem don't exit if error happened
+:exit
