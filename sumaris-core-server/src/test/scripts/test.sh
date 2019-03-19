e4a76f15e73328d2789d4f32c37d441e95e21ada
blavenie <benoit.lavenier@e-is.pro>
2019-03-19 15:32:17 +0100

    [enh] rename modules core-server and importation (into server and core-importation) [fix] Remove unused dependency hibernate-tools

--- sumaris-core-server/src/test/scripts/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-curl -XPOST 'http://localhost:7777/graphql' -H "Content-Type: application/json" -d '
-{
-"operationName": null,
-    "variables": {},
-  "query":"{\n  trips {\n    id\n    departureDateTime\n    returnDateTime\n    comments\n    creationDate\n    __typename\n  }\n}"
-}'
