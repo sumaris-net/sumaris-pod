604d73cb1494f07593cc334cc9c860ed35d06a7a
Benoit Lavenier <benoit.lavenier@e-is.pro>
2019-11-15 14:00:28 +0100

    [fix] Liquibase: minor changes in SQL comments

--- sumaris-core/src/main/resources/net/sumaris/core/db/changelog/hsqldb/all/insert_qualitative_values.sql
+++ sumaris-core/src/main/resources/net/sumaris/core/db/changelog/hsqldb/all/insert_qualitative_values.sql
@@ -3,7 +3,7 @@
 -- Insertion des valeurs qualitatives (pour les coef de conversion)
 --
 -- *****************************************************************
--- Requête de generation, à executer sur le schéma SIH2_ADAGIO_DBA:
+-- Requête ayant permis la generation des INSERT, à executer sur le schéma SIH2_ADAGIO_DBA:
 --
 -- ( select distinct 'insert into QUALITATIVE_VALUE (id, status_fk, creation_date, update_date, label, name, parameter_fk) ',
 --                     'select NEXT VALUE FOR QUALITATIVE_VALUE_SEQ, 1, sysdate, now, ',
