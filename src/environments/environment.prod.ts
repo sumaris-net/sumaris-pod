403bf196b2917e564116bf82e70ec17590e2280d
Benoit Lavenier <benoit.lavenier@e-is.pro>
2020-11-23 16:19:11 +0100

    [fix] Change pod version to 1.5.0

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -13,7 +13,7 @@ export const environment: Environment = Object.freeze({
   mock: false,
 
   // Must be change manually. Can be override using Pod properties 'sumaris.app.min.version'
-  peerMinVersion: '1.4.6',
+  peerMinVersion: '1.5.0',
 
   // FIXME: GraphQL subscription never unsubscribe...
   listenRemoteChanges: false,
