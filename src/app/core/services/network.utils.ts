a0d8dbcb2e35a9caed96008f42c176b3e8a0f230
Benoit Lavenier <benoit.lavenier@e-is.pro>
2020-11-24 09:31:34 +0100

    [fix] Remove HTTP native client

--- src/app/core/services/network.utils.ts
+++ src/app/core/services/network.utils.ts
@@ -1,6 +1,7 @@
 import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {HTTP} from "@ionic-native/http/ngx";
 import {HttpUtils} from "../../shared/http/http.utils";
+import {Peer} from "./model/peer.model";
 
 export interface NodeInfo {
   softwareName: string;
@@ -15,16 +16,19 @@ export class NetworkUtils {
 
 export async function getNodeInfo(
   http: HTTP | HttpClient,
-  peerUrl: string,
+  peer: string|Peer,
   opts?: {
   headers?: HttpHeaders | {
     [header: string]: string | string[];
   };
 }): Promise<NodeInfo> {
 
+  if (!peer) throw Error("Missing argument 'peer'.");
+
+  let peerUrl = (peer instanceof Peer) ? peer.url : (peer as string);
   // Remove trailing slash
   if (peerUrl.endsWith('/')) {
-    peerUrl = peerUrl.substr(0, peerUrl.length - 1);
+    peerUrl = peerUrl.substr(0, peerUrl.length -1);
   }
 
   return HttpUtils.getResource(http, peerUrl + '/api/node/info', opts);
--- src/app/shared/http/http.utils.ts
+++ src/app/shared/http/http.utils.ts
@@ -1,61 +1,54 @@
 import {HttpClient, HttpHeaders, HttpParams} from "@angular/common/http";
 import {HTTP} from "@ionic-native/http/ngx";
-import {environment} from "../../../environments/environment";
 
-export interface NodeInfo {
-  softwareName: string;
-  softwareVersion: string;
-  nodeLabel?: string;
-  nodeName?: string;
-}
 
 export class HttpUtils {
-  static getResource = getResource;
-}
+  static async getResource<T>(http: HttpClient | HTTP,
+                              uri: string,
+                              opts?: {
+                                headers?: HttpHeaders | {
+                                  [header: string]: string | string[];
+                                };
+                                params?: HttpParams | {
+                                  [param: string]: string | string[];
+                                };
+                                reportProgress?: boolean;
+                                responseType?: 'json';
+                                withCredentials?: boolean;
+                              }): Promise<T> {
 
-export async function getResource<T>(http: HttpClient | HTTP,
-                                     uri: string,
-                                     opts?: {
-    headers?: HttpHeaders | {
-      [header: string]: string | string[];
-    };
-    params?: HttpParams | {
-      [param: string]: string | string[];
-    };
-    reportProgress?: boolean;
-    responseType?: 'json';
-    withCredentials?: boolean;
-  }): Promise<T> {
-
-  // Add headers
-  opts = {
-    headers: new HttpHeaders(),
+    // Add headers
+    opts = {
+      //headers: new HttpHeaders(),
       //.append('X-App-Name', environment.name)
       //.append('X-App-Version', environment.version),
-    ...opts
-  };
+      ...opts
+    };
 
-  try {
+    try {
 
-    // Use native http client
-    if (http instanceof HTTP) {
-      // Execute the request
-      const response = await http.get(uri, opts.params, opts.headers);
-      console.info("[network] response: " + response.data);
-      return response.data as T;
-    }
+      // Use native http client
+      if (http instanceof HTTP) {
+        // Execute the request
+        const response = await http.get(uri, opts.params, opts.headers);
+        console.info("[network] response: " + response.data);
+        return response.data as T;
+      }
 
-    // Using web http client
-    return (await http.get(uri, opts).toPromise()) as T;
-  }
-  catch (err) {
-    if (err && err.message) {
-      console.error(`[network] Error on get request ${uri}: ${err.message}`, err);
+      // Using web http client
+      return (await http.get(uri, opts).toPromise()) as T;
     }
-    else {
-      console.error(`[network] Error on get request ${uri}: ${err && err.statusText}`);
+    catch (err) {
+      if (err && err.message) {
+        console.error(`[network] Error on get request ${uri}: ${err.message}`, err);
+      }
+      else {
+        console.error(`[network] Error on get request ${uri}: ${err && err.statusText}`);
+      }
+      throw {code: err.status, message: "ERROR.UNKNOWN_NETWORK_ERROR"};
     }
-    throw {code: err.status, message: "ERROR.UNKNOWN_NETWORK_ERROR"};
   }
 }
+
+
 
