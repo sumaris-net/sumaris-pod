3a5fb10ace1eaf7a38a0bc9140eddf7d7815ab5f
BRUVRY-LAGADEC <leo.bruvry.lagadec@ifremer.fr>
2021-01-05 17:13:29 +0100

    properly init ids for appliedStrategy and strategyDepartment

--- src/app/referential/services/strategy.service.ts
+++ src/app/referential/services/strategy.service.ts
@@ -129,6 +129,7 @@ export const StrategyFragments = {
   `,
   appliedStrategy: gql`
     fragment AppliedStrategyFragment on AppliedStrategyVO {
+      id
       strategyId
       location {
         ...ReferentialFragment
@@ -150,6 +151,7 @@ export const StrategyFragments = {
   `,
   strategyDepartment: gql`
     fragment StrategyDepartmentFragment on StrategyDepartmentVO {
+      id
       strategyId
       location {
         ...ReferentialFragment
