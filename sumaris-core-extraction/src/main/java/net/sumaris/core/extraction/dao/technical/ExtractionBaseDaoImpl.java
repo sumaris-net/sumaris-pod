98597fb6bf41833a83a04a03bead2f4645fc2377
Nolwenn Rannou <nolwenn.rannou@partenaire-exterieur.ifremer.fr>
2021-02-01 16:14:03 +0100

    [fix] Extraction program  fix oracle compatibility1

--- sumaris-core-extraction/src/main/java/net/sumaris/core/extraction/dao/technical/ExtractionBaseDaoImpl.java
+++ sumaris-core-extraction/src/main/java/net/sumaris/core/extraction/dao/technical/ExtractionBaseDaoImpl.java
@@ -61,6 +61,7 @@ public abstract class ExtractionBaseDaoImpl extends HibernateDaoSupport {
     private static final Logger log = LoggerFactory.getLogger(ExtractionBaseDaoImpl.class);
 
     protected static final String XML_QUERY_PATH = "xmlQuery";
+    protected static final String XSL_ORACLE_FILENAME = "xmlQuery/queryOracle.xsl";
 
     @Autowired
     protected SumarisConfiguration configuration;
@@ -175,7 +176,11 @@ public abstract class ExtractionBaseDaoImpl extends HibernateDaoSupport {
      * @return
      */
     protected XMLQuery createXMLQuery() {
-        return applicationContext.getBean("xmlQuery", XMLQuery.class);
+        XMLQuery xmlQuery = applicationContext.getBean("xmlQuery", XMLQuery.class);
+        if (this.databaseType == DatabaseType.oracle) {
+            xmlQuery.setXSLFileName(XSL_ORACLE_FILENAME);
+        }
+        return xmlQuery;
     }
 
     protected <C extends ExtractionContextVO> void clean(@NonNull C context) {
