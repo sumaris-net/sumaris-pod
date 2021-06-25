8bdee67e1b3ac74267175967f08e7c8a39e861b6
bl05b3e <benoit.lavenier@e-is.pro>
2021-06-25 14:10:27 +0200

    [fix] CI: fix yaml

--- sumaris-server/src/main/docker/app.sh
+++ sumaris-server/src/main/docker/app.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-SCRIPT_DIR=$(dirname $0)
-BASEDIR=$(cd "${SCRIPT_DIR}" && pwd -P)
-
+BASEDIR=/app
 JAVA_OPTS="-Dsumaris.basedir=${BASEDIR}"
-JAVA_OPTS="$JAVA_OPTS -Dspring.config.location=${BASEDIR}/config/"
-JAVA_OPTS="$JAVA_OPTS -Doracle.net.tns_admin=/home/tnsnames"
-JAVA_OPTS="$JAVA_OPTS -Doracle.jdbc.timezoneAsRegion=false"
-[[ "_${PROFILES}" != "_" ]] && JAVA_OPTS="$JAVA_OPTS -Dspring.profiles.active=${PROFILES}"
-[[ "_${TZ}" != "_" ]] && JAVA_OPTS="$JAVA_OPTS -Duser.timezone=${TZ}"
-[[ "_${PORT}" -ne "_" ]] && JAVA_OPTS="$JAVA_OPTS -Dserver.port=${PORT}"
-ARGS=${@:2}
-JAVA_CMD="java ${JAVA_OPTS} -server -jar ${BASEDIR}/app.war ${ARGS}"
+JAVA_OPTS="${JAVA_OPTS} -Dspring.config.location=${BASEDIR}/config/"
+JAVA_OPTS="${JAVA_OPTS} -Doracle.net.tns_admin=/home/tnsnames"
+JAVA_OPTS="${JAVA_OPTS} -Doracle.jdbc.timezoneAsRegion=false"
+[[ "_${PROFILES}" != "_" ]] && JAVA_OPTS="${JAVA_OPTS} -Dspring.profiles.active=${PROFILES}"
+[[ "_${TZ}" != "_" ]] && JAVA_OPTS="${JAVA_OPTS} -Duser.timezone=${TZ}"
+[[ "_${PORT}" != "_" ]] && JAVA_OPTS="${JAVA_OPTS} -Dserver.port=${PORT}"
+ARGS=
+# TODO test this
+#ARGS=${@:2}
 
-echo "Starting, with arguments: ${ARGS} - profiles: ${PROFILES}"
-echo " > ${CMD}"
-${JAVA_CMD}
+echo "*** Starting Application - args: ${ARGS} - profiles: ${PROFILES} ***"
+
+java ${JAVA_OPTS} -server -jar ${BASEDIR}/app.war ${ARGS}
