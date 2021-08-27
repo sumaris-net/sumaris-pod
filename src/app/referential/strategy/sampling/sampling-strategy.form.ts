27fb21e35f4c5a7d3654172fe15a141f25ac1fa8
msevere <mael.severe@altran.com>
2021-08-27 11:48:32 +0200

    [FIX] Imagine515 - allow user to edit label with increment already set

--- src/app/referential/strategy/sampling/sampling-strategy.form.ts
+++ src/app/referential/strategy/sampling/sampling-strategy.form.ts
@@ -948,7 +948,7 @@ export class SamplingStrategyForm extends AppForm<Strategy> implements OnInit {
   protected async onEditLabel(value: string) {
     const taxonNameControl = this.taxonNamesHelper.at(0);
     if (!value) return
-    const expectedLabelFormatRegex = new RegExp(/^\d\d [a-zA-Z][a-zA-Z][a-zA-Z][a-zA-Z][a-zA-Z][a-zA-Z][a-zA-Z] ___/);
+    const expectedLabelFormatRegex = new RegExp(/^\d\d [a-zA-Z][a-zA-Z][a-zA-Z][a-zA-Z][a-zA-Z][a-zA-Z][a-zA-Z] ___$/);
     if (value.match(expectedLabelFormatRegex)) {
       const currentViewTaxon = taxonNameControl?.value?.taxonName;
       const isUnique = await this.isTaxonNameUnique(value.substring(3, 10), currentViewTaxon?.id);
@@ -959,6 +959,18 @@ export class SamplingStrategyForm extends AppForm<Strategy> implements OnInit {
         const computedLabel = this.program && (await this.strategyService.computeNextLabel(this.program.id, value.substring(0, 10).replace(' ', '').toUpperCase(), 3));
         const labelControl = this.form.get('label');
         labelControl.setValue(computedLabel);
+      }
+    }
+    const acceptedLabelFormatRegex = new RegExp(/^\d\d [a-zA-Z][a-zA-Z][a-zA-Z][a-zA-Z][a-zA-Z][a-zA-Z][a-zA-Z] \d\d\d$/);
+    if (value.match(acceptedLabelFormatRegex)) {
+      const currentViewTaxon = taxonNameControl?.value?.taxonName;
+      const isUnique = await this.isTaxonNameUnique(value.substring(3, 10), currentViewTaxon?.id);
+      if (!isUnique) {
+        taxonNameControl.setErrors({ uniqueTaxonCode: true });
+      } else {
+        SharedValidators.clearError(this.taxonNamesHelper.at(0), 'uniqueTaxonCode');
+        const labelControl = this.form.get('label');
+        labelControl.setValue(value.replace(' ', '').toUpperCase());
       }
     }
   }
