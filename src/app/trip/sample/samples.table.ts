59d88f2d95b40670342b0a842debb790d1ac8778
BRUVRY-LAGADEC <leo.bruvry.lagadec@ifremer.fr>
2021-11-25 16:03:25 +0100

    formating things

--- src/app/trip/sample/samples.table.ts
+++ src/app/trip/sample/samples.table.ts
@@ -44,6 +44,7 @@ import { BehaviorSubject, Subscription } from 'rxjs';
 import { MatMenu } from '@angular/material/menu';
 import { TaxonNameRef } from '@app/referential/services/model/taxon-name.model';
 import { isNilOrNaN } from '@app/shared/functions';
+import { DenormalizedPmfmStrategy } from '@app/referential/services/model/pmfm-strategy.model';
 
 const moment = momentImported;
 
@@ -647,11 +648,11 @@ export class SamplesTable extends AppMeasurementsTable<Sample, SampleFilter> {
         + (this.showLabelColumn ? 1 : 0)
         + (this.showTaxonGroupColumn ? 1 : 0)
         + (this.showTaxonNameColumn ? 1 : 0)
-        + (this.showDateTimeColumn ? 1 : 0)
+        + (this.showDateTimeColumn ? 1 : 0);
       this.groupHeaderEndColSpan = RESERVED_END_COLUMNS.length
-        + (this.showCommentsColumn ? 1 : 0)
+        + (this.showCommentsColumn ? 1 : 0);
 
-      pmfms  = orderedPmfms
+    pmfms  = orderedPmfms;
     }
     else {
       // Apply weight conversion
