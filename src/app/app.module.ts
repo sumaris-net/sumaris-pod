c16dfb8ab97ac6670a89d94b2a55b561ed1b7e03
pyclement <pierre-yves.clement@altran.com>
2021-11-29 10:25:53 +0100

    [fix] fix import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,30 @@
 import './vendor';
 
-import { APP_BASE_HREF } from '@angular/common';
-import { BrowserModule, HAMMER_GESTURE_CONFIG, HammerModule } from '@angular/platform-browser';
-import { CUSTOM_ELEMENTS_SCHEMA, NgModule, SecurityContext } from '@angular/core';
-import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
-import { MomentDateAdapter } from '@angular/material-moment-adapter';
+import {APP_BASE_HREF} from '@angular/common';
+import {BrowserModule, HAMMER_GESTURE_CONFIG, HammerModule} from '@angular/platform-browser';
+import {CUSTOM_ELEMENTS_SCHEMA, NgModule, SecurityContext} from '@angular/core';
+import {DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE} from '@angular/material/core';
+import {MomentDateAdapter} from '@angular/material-moment-adapter';
 
 // Ionic plugins
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
-import { Keyboard } from '@ionic-native/keyboard/ngx';
-import { NativeAudio } from '@ionic-native/native-audio/ngx';
-import { Vibration } from '@ionic-native/vibration/ngx';
-import { ImagePicker } from '@ionic-native/image-picker/ngx';
-import { MediaCapture } from '@ionic-native/media-capture/ngx';
-import { PhotoViewer } from '@ionic-native/photo-viewer/ngx';
-import { File } from '@ionic-native/file/ngx';
+import {SplashScreen} from '@ionic-native/splash-screen/ngx';
+import {StatusBar} from '@ionic-native/status-bar/ngx';
+import {Keyboard} from '@ionic-native/keyboard/ngx';
+import {NativeAudio} from '@ionic-native/native-audio/ngx';
+import {Vibration} from '@ionic-native/vibration/ngx';
+import {ImagePicker} from '@ionic-native/image-picker/ngx';
+import {MediaCapture} from '@ionic-native/media-capture/ngx';
+import {PhotoViewer} from '@ionic-native/photo-viewer/ngx';
+import {File} from '@ionic-native/file/ngx';
 
 // App modules
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
+import {AppComponent} from './app.component';
+import {AppRoutingModule} from './app-routing.module';
 import {
   APP_ABOUT_DEVELOPERS,
   APP_ABOUT_PARTNERS,
   APP_CONFIG_OPTIONS,
+  APP_FORM_ERROR_I18N_KEYS,
   APP_GRAPHQL_TYPE_POLICIES,
   APP_HOME_BUTTONS,
   APP_LOCAL_SETTINGS,
@@ -43,33 +44,32 @@ import {
   SocialModule,
   TestingPage,
 } from '@sumaris-net/ngx-components';
-import { environment } from '@environments/environment';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { Camera } from '@ionic-native/camera/ngx';
-import { Network } from '@ionic-native/network/ngx';
-import { AudioManagement } from '@ionic-native/audio-management/ngx';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { TRIP_CONFIG_OPTIONS, TRIP_GRAPHQL_TYPE_POLICIES, TRIP_LOCAL_SETTINGS_OPTIONS, TRIP_STORAGE_TYPE_POLICIES } from './trip/services/config/trip.config';
-import { IonicStorageModule } from '@ionic/storage';
-import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
-import { IonicModule } from '@ionic/angular';
-import { CacheModule } from 'ionic-cache';
-import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
-import { MarkdownModule, MarkedOptions } from 'ngx-markdown';
-import { TypePolicies } from '@apollo/client/core';
-import { TRIP_TESTING_PAGES } from './trip/trip.testing.module';
-import { EXTRACTION_CONFIG_OPTIONS, EXTRACTION_GRAPHQL_TYPE_POLICIES } from './extraction/services/config/extraction.config';
-import { REFERENTIAL_CONFIG_OPTIONS, REFERENTIAL_GRAPHQL_TYPE_POLICIES, REFERENTIAL_LOCAL_SETTINGS_OPTIONS } from './referential/services/config/referential.config';
-import { DATA_CONFIG_OPTIONS, DATA_GRAPHQL_TYPE_POLICIES } from './data/services/config/data.config';
-import { VESSEL_CONFIG_OPTIONS, VESSEL_GRAPHQL_TYPE_POLICIES, VESSEL_LOCAL_SETTINGS_OPTIONS } from './vessel/services/config/vessel.config';
-import { JDENTICON_CONFIG } from 'ngx-jdenticon';
-import { REFERENTIAL_TESTING_PAGES } from './referential/referential.testing.module';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { AppSharedModule } from '@app/shared/shared.module';
-import { APP_CORE_CONFIG_OPTIONS } from '@app/core/services/config/core.config';
-import { AppCoreModule } from '@app/core/core.module';
-import { APP_FORM_ERROR_I18N_KEYS } from '../../ngx-sumaris-components/src/app/shared/validator/form-error-adapter.class';
-import { SAMPLE_VALIDATOR_I18N_ERROR_KEYS } from '@app/trip/services/validator/sample.validator';
+import {environment} from '@environments/environment';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import {Camera} from '@ionic-native/camera/ngx';
+import {Network} from '@ionic-native/network/ngx';
+import {AudioManagement} from '@ionic-native/audio-management/ngx';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {TRIP_CONFIG_OPTIONS, TRIP_GRAPHQL_TYPE_POLICIES, TRIP_LOCAL_SETTINGS_OPTIONS, TRIP_STORAGE_TYPE_POLICIES} from './trip/services/config/trip.config';
+import {IonicStorageModule} from '@ionic/storage';
+import {InAppBrowser} from '@ionic-native/in-app-browser/ngx';
+import {IonicModule} from '@ionic/angular';
+import {CacheModule} from 'ionic-cache';
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import {MarkdownModule, MarkedOptions} from 'ngx-markdown';
+import {TypePolicies} from '@apollo/client/core';
+import {TRIP_TESTING_PAGES} from './trip/trip.testing.module';
+import {EXTRACTION_CONFIG_OPTIONS, EXTRACTION_GRAPHQL_TYPE_POLICIES} from './extraction/services/config/extraction.config';
+import {REFERENTIAL_CONFIG_OPTIONS, REFERENTIAL_GRAPHQL_TYPE_POLICIES, REFERENTIAL_LOCAL_SETTINGS_OPTIONS} from './referential/services/config/referential.config';
+import {DATA_CONFIG_OPTIONS, DATA_GRAPHQL_TYPE_POLICIES} from './data/services/config/data.config';
+import {VESSEL_CONFIG_OPTIONS, VESSEL_GRAPHQL_TYPE_POLICIES, VESSEL_LOCAL_SETTINGS_OPTIONS} from './vessel/services/config/vessel.config';
+import {JDENTICON_CONFIG} from 'ngx-jdenticon';
+import {REFERENTIAL_TESTING_PAGES} from './referential/referential.testing.module';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import {AppSharedModule} from '@app/shared/shared.module';
+import {APP_CORE_CONFIG_OPTIONS} from '@app/core/services/config/core.config';
+import {AppCoreModule} from '@app/core/core.module';
+import {SAMPLE_VALIDATOR_I18N_ERROR_KEYS} from '@app/trip/services/validator/sample.validator';
 
 
 @NgModule({
