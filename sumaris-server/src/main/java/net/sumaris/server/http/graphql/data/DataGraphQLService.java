3b94bf23c147ba01ba95dd1eba694c861537c577
bl05b3e <benoit.lavenier@e-is.pro>
2022-09-07 20:05:15 +0200

    [fix] Remove unecessary @IsUser ina sub graphQL query

--- sumaris-server/src/main/java/net/sumaris/server/http/graphql/data/DataGraphQLService.java
+++ sumaris-server/src/main/java/net/sumaris/server/http/graphql/data/DataGraphQLService.java
@@ -782,8 +782,6 @@ public class DataGraphQLService {
     /* -- Operation Groups -- */
 
     @GraphQLQuery(name = "operationGroups", description = "Get trip's operation groups")
-    @Transactional(readOnly = true)
-    @IsUser
     public List<OperationGroupVO> getOperationGroupsByTrip(@GraphQLContext TripVO trip) {
         if (trip.getOperationGroups() != null) return trip.getOperationGroups();
         if (trip.getId() == null) return null;
