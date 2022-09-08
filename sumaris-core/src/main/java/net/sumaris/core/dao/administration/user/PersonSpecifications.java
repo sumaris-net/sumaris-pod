b0e4c6f59a2f651745328e5236846e406e8c2477
bl05b3e <benoit.lavenier@e-is.pro>
2022-09-08 18:44:55 +0200

    [enh] Optimize conversion of observers into PersonVO (use a cached method)

--- sumaris-core/src/main/java/net/sumaris/core/dao/administration/user/PersonSpecifications.java
+++ sumaris-core/src/main/java/net/sumaris/core/dao/administration/user/PersonSpecifications.java
@@ -40,6 +40,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 
+import javax.annotation.Nullable;
 import javax.persistence.criteria.ParameterExpression;
 import java.util.Arrays;
 import java.util.Collection;
