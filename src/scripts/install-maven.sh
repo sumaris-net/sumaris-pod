13f7b2db634c72e5ed7704a99b06b6a793970a2d
bl05b3e <benoit.lavenier@e-is.pro>
2022-12-14 11:03:42 +0100

    [enh] Add maven install script

--- src/scripts/install-maven.sh
+++ src/scripts/install-maven.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-export VER="3.8.6"
-INSTALL_DIR="/opt/apache-maven-${VER}"
+export MAVEN_VERSION="3.8.6"
 
-if [[ -d "/opt/apache-maven-${VER}" && -f "/etc/profile.d/maven.sh" ]]; then
-  echo "Apache Maven ${VER} already installed, at '/opt/apache-maven-${VER}'"
+if [[ -d "/opt/apache-maven-${MAVEN_VERSION}" && -f "/etc/profile.d/maven.sh" ]]; then
+  echo "Apache Maven ${MAVEN_VERSION} already installed, at '/opt/apache-maven-${MAVEN_VERSION}'"
 
   source /etc/profile.d/maven.sh
   exit 0
 fi
 
-wget http://www-eu.apache.org/dist/maven/maven-3/${VER}/binaries/apache-maven-${VER}-bin.tar.gz -P /tmp
+wget http://www-eu.apache.org/dist/maven/maven-3/${MAVEN_VERSION}/binaries/apache-maven-${MAVEN_VERSION}-bin.tar.gz -P /tmp
 [[ $? -ne 0 ]] && exit 1
 
 if [[ -e "/opt/maven" ]]; then
@@ -19,10 +18,10 @@ if [[ -e "/opt/maven" ]]; then
   [[ $? -ne 0 ]] && exit 1
 fi
 
-sudo tar xvf apache-maven-${VER}-bin.tar.gz -C /opt
+sudo tar xvf apache-maven-${MAVEN_VERSION}-bin.tar.gz -C /opt
 [[ $? -ne 0 ]] && exit 1
 
-sudo ln -s /opt/apache-maven-${VER} /opt/maven
+sudo ln -s /opt/apache-maven-${MAVEN_VERSION} /opt/maven
 [[ $? -ne 0 ]] && exit 1
 
 if [[ -e "/etc/profile.d/maven.sh" ]]; then
@@ -31,7 +30,6 @@ if [[ -e "/etc/profile.d/maven.sh" ]]; then
 fi
 
 cat <<EOF | sudo tee /etc/profile.d/maven.sh
-export JAVA_HOME=/usr/lib/jvm/default-java
 export M2_HOME=/opt/maven
 export MAVEN_HOME=/opt/maven
 export PATH=\${M2_HOME}/bin:\${PATH}
