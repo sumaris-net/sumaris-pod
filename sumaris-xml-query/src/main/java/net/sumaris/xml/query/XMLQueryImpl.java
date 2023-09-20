3a1b43c090391f7d6e0fd87a3053aa3c8bcc5712
bl05b3e <benoit.lavenier@e-is.pro>
2023-09-20 11:50:18 +0200

    [wip] Extraction ObsMer: avoid error duplicated column

--- sumaris-xml-query/src/main/java/net/sumaris/xml/query/XMLQueryImpl.java
+++ sumaris-xml-query/src/main/java/net/sumaris/xml/query/XMLQueryImpl.java
@@ -41,6 +41,7 @@ import org.jdom2.Attribute;
 import org.jdom2.Document;
 import org.jdom2.Element;
 import org.jdom2.Text;
+import org.jdom2.filter.Filter;
 import org.jdom2.filter.Filters;
 import org.jdom2.output.Format;
 import org.jdom2.util.IteratorIterable;
