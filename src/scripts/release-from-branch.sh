8ae1379da5da440e99210a42f67d0c4ec22b20aa
bl05b3e <benoit.lavenier@e-is.pro>
2024-02-09 17:33:38 +0100

    Release 2.9.0-rc3\n

--- src/scripts/release-from-branch.sh
+++ src/scripts/release-from-branch.sh
@@ -49,7 +49,8 @@ mvn versions:set -DnewVersion=$version && mvn versions:commit
 [[ $? -ne 0 ]] && exit 1
 echo "---- Prepare release [OK]"
 echo ""
-git branch -B "release/$version"
+
+git branch -f "release/$version"
 [[ $? -ne 0 ]] && exit 1
 
 echo "---- Performing release..."
