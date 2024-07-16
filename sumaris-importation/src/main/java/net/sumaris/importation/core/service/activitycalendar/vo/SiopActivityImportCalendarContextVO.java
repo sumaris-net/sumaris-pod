6eeab0f5dc5b13c105be24a2c6fd731b96ee6b31
Morgan Facorat <m.facorat@codra.fr>
2024-07-16 14:28:58 +0200

    enh(calendar) csv import init

---
+++ sumaris-importation/src/main/java/net/sumaris/importation/core/service/activitycalendar/vo/SiopActivityImportCalendarContextVO.java
@@ -0,0 +1,24 @@
+package net.sumaris.importation.core.service.activitycalendar.vo;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NonNull;
+
+import java.io.File;
+
+@Data
+@Builder
+public class SiopActivityImportCalendarContextVO {
+    @NonNull
+    private Integer recorderPersonId;
+
+    @NonNull
+    private File processingFile;
+
+    // result object containing messages and errors during process
+    @NonNull
+    @Builder.Default
+    @JsonIgnore
+    private SiopActivityCalendarImportResultVO result = new SiopActivityCalendarImportResultVO();
+}
